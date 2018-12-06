9e5d7358a781053165831b3c98e6853a2bc7ea41
wll <wllnyubupt@gmail.com>
2018-12-06 09:15:24 -0500

    divide load and search

--- Data_process/word2vec.py
+++ Data_process/word2vec.py
@@ -1,12 +1,11 @@
 import numpy as np
 
-def word2vec(glove_path ,word):
+def load_glove(glove_path):
     """
-      Arguments:
-                glove_path: the path storing the downloaded glove.6B.50d.txt
-                word: the word waiting for transfering to the GloVe vector
-      Outputs: 
-                the corresponding GloVe vector for it
+    Argument: 
+             glove_path: the path storing the downloaded glove.6B.50d.txt
+    Output:
+             a dictionary like {key: word, value: glove vector}         
     """
     # read in the whole glove and store as a dictionay
     # {key: word, value: glove vector}
@@ -14,10 +13,22 @@ def word2vec(glove_path ,word):
         glove = {line.split()[0]: np.fromiter(map(float, line.split()[1:]),dtype=np.float) 
              for line in fin}
 
+    return glove
+
+def word2vec(glove, word):
+    """
+      Arguments:
+                glove: the glove dictionary like {key: word, value: glove vector} 
+                word: the word waiting for transfering to the GloVe vector
+      Outputs: 
+                the corresponding GloVe vector for it
+    """
+    
     #print(list(glove.keys()))
 
     return glove[word]
 
 if __name__ == "__main__":
 
-    print( word2vec('../data/glove.6B/glove.6B.50d.txt', 'the'))
+    glove_vec = load_glove('../data/glove.6B/glove.6B.50d.txt')
+    print( word2vec(glove_vec, 'the'))
