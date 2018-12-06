1c78aaa1f436fed06f6db64d5c5768b2b4a2cc2d
wll <wllnyubupt@gmail.com>
2018-12-06 17:40:58 -0500

    add compute avg wv

---
+++ SVM/compute_average_wordvec.py
@@ -0,0 +1,100 @@
+"""
+Compute average word vector for each context
+"""
+
+import re
+import json
+import codecs
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+
+from process_data import clean_data
+from process_data import tokenize
+
+def compute_average_wordvec(dataset_path, embedding_size=50):
+    """
+     Argument:
+             dataset_path: the folder name storing amazon or yelp dataset,
+                           like '../data/Amazon' or '../data/Yelp'
+             embedding_size: the dimension of word vector
+     Output:
+             .csv files containing (label, average word vector)                      
+    """    
+    # get the full paths, word2idx and embedding matrix for each dataset
+    is_amazon = re.search(r'Amazon', dataset_path)
+    if is_amazon is not None:
+        dataset_paths = [dataset_path + "/amazon.train.csv",\
+                         dataset_path + "/amazon.valid.csv",\
+                         dataset_path + "/amazon.test.csv"]
+        word2idx_file = "/amazon.vacab.dict.json"
+        embedding_matrix_file = "/amazon.w2v.matrix.json"            
+    else:
+        dataset_paths = [dataset_path + "/yelp.train.csv",\
+                         dataset_path + "/yelp.valid.csv",\
+                         dataset_path + "/yelp.test.csv"]    
+        word2idx = json.load(dataset_path + "/yelp.vacab.dict.json")                 
+        word2idx_file = "/yelp.vacab.dict.json"
+        embedding_matrix_file = "/yelp.w2v.matrix.json"
+        
+
+        # load  the word2idx json file
+        word2idx = json.load(dataset_path + word2idx_file)
+        # load the embedding matrix file
+        obj_text = codecs.open(dataset_path + embedding_matrix_file, 'r', encoding='utf-8').read()
+        embedding_matrix = json.loads(obj_text)
+        embedding_matrix = np.array(embedding_matrix)
+
+    # read three files clean and tokenize them
+    # then get the average vector for each review. 
+    for file in dataset_paths:
+        data = pd.read_csv(file, header=None, low_memory=False) 
+        # the first column is label, the second one is context
+        label_list = np.asarray(data.iloc[:, 0])
+        context_list = np.asarray(data.iloc[:, 1])
+        # get the average word vector representation for each sample
+        average_vec_list = []
+        for context in context_list:
+            count = 0
+            sum_vec = np.zeros(embedding_size)
+            for token in tokenize(clean_data(context)):
+                index = word2idx[token]
+                sum_vec += embedding_matrix[index, :]
+                count += 1
+            average_vec_list.append(sum_vec / count)    
+
+        # write the whole data into a .csv file.
+        # determine the output file name
+        if re.search(r'amazon', file) is not None:
+            if re.search(r'train', file) is not None:
+                output_file = "/amazon.train.vector.csv"
+            elif re.search(r'valid') is not None:
+                output_file = "/amazon.valid.vector.csv"
+            else:
+                output_file = "/amazon.test.vector.csv"
+        else:
+            if re.search(r'train', file) is not None:
+                output_file = "/yelp.train.vector.csv"
+            elif re.search(r'valid') is not None:
+                output_file = "/yelp.valid.vector.csv"
+            else:
+                output_file = "/yelp.test.vector.csv"
+
+        sample_df = pd.DataFrame()
+        sample_df['label'] = label_list
+        sample_df['avg_vector'] = average_vec_list
+        sample_df.to_csv(dataset_path + output_file, encoding='utf-8', index=False)
+
+
+if __name__ =="__main__":
+
+    datasets = ['../data/Amazon', '../data/Yelp']
+    for dataset in datasets:
+        compute_average_wordvec(dataset)
+
+
+
+                        
+    
+     
+
