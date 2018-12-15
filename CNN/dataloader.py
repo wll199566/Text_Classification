662cae44cf3f1569c99b9416624d1bcca189e41a
sysumathcoder <547988668@qq.com>
2018-12-15 13:48:34 -0500

    change dataloader

--- CNN/dataloader.py
+++ CNN/dataloader.py
@@ -6,27 +6,35 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 class Vocab(torch.utils.data.Dataset):
-    def __init__(self, root="./data/yelp.cleaned.datasets", max_size=3000):
+    def __init__(self, root="./data/yelp.cleaned.datasets", max_size=3000, load_val=True):
         self.spacy_en = spacy.load('en')
         self.TEXT = data.Field(tokenize=self.tokenizer)
         self.LABEL = data.Field(sequential=False, use_vocab=False, dtype=torch.float)
         tv_dataFields = [("label", self.LABEL), ("text", self.TEXT)]
-        self.trn, self.val = data.TabularDataset.splits(path=root, train='train.csv',
-                                                        validation="valid.csv", format='csv',
-                                                        skip_header=True, fields=tv_dataFields)
+
+        self.load_val = load_val
+        if self.load_val:
+            self.trn, self.val = data.TabularDataset.splits(path=root, train='train.csv',
+                                                            validation="valid.csv", format='csv',
+                                                            skip_header=True, fields=tv_dataFields)
+        else:
+            self.trn, _ = data.TabularDataset.splits(path=root, train='train.csv',
+                                                     format='csv',
+                                                     skip_header=True, fields=tv_dataFields)
+            self.val = None
 
         # Print to test
-        print("init Vocab success!")
-        print("val[0].__dict__: ", self.val[0].__dict__['label'])
-        print("val[0].['text']: ", self.val[0].__dict__['text'][:10])
+        # print("init Vocab success!")
+        # print("val[0].__dict__: ", self.val[0].__dict__['label'])
+        # print("val[0].['text']: ", self.val[0].__dict__['text'][:10])
 
         self.TEXT.build_vocab(self.trn, max_size=max_size, vectors="glove.6B.50d")
 
     def __getitem__(self, index):
-        return self.trn[index], self.val[index]
+        return self.trn[index]  # , self.val[index]
 
     def __len__(self):
-        return len(self.trn) + len(self.val)
+        return len(self.trn)  # + len(self.val)
 
     def tokenizer(self, text):
         token = [t.text for t in self.spacy_en.tokenizer(text)]
@@ -36,8 +44,8 @@ class Vocab(torch.utils.data.Dataset):
         return token
 
 
-def data_loader(root="./data/yelp.cleaned.datasets", max_size=3000):
-    vocab = Vocab(root, max_size)
+def data_loader(root="./data/yelp.cleaned.datasets", max_size=3000, load_val=True, load_train=True):
+    vocab = Vocab(root, max_size, load_val=True)
     # train_iterator, valid_iterator = data.BucketIterator.splits(
     #     (vocab.trn, vocab.val),
     #     batch_size=batch_size,
@@ -46,4 +54,9 @@ def data_loader(root="./data/yelp.cleaned.datasets", max_size=3000):
     #     # BucketIterator 依据什么对数据分组
     #     sort_within_batch=True)
 
-    return vocab.TEXT.vocab, vocab.trn, vocab.val
+    if load_val and load_train:
+        return vocab.TEXT.vocab, vocab.trn, vocab.val
+    elif load_train:
+        return vocab.TEXT.vocab, vocab.trn, None
+    else:
+        return vocab.TEXT.vocab, None, None
