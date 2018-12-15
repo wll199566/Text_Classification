56830114b80236f67c27229396df27e06e048510
sysumathcoder <547988668@qq.com>
2018-12-15 17:49:31 -0500

    SVM for features

--- CNN/svm.py
+++ CNN/svm.py
@@ -8,6 +8,7 @@ import numpy as np
 from sklearn import svm
 from sklearn import metrics
 
+
 def read_dataset_file(file_path):
     """
      Argument:
@@ -19,22 +20,23 @@ def read_dataset_file(file_path):
     """
     samples = []
     target_labels = []
-    
+
     with open(file_path, 'rt') as fin:
         for i, row in enumerate(fin, 1):
-            #if i == 3:
+            # if i == 3:
             #    break
             sample_dict = json.loads(row)
             # Note to check whether all the numbers are finite.
             if np.all(np.isfinite(sample_dict['avg_vec'])):
                 samples.append(sample_dict['avg_vec'])
                 target_labels.append(int(sample_dict['label']))
-    
+
     # convert the list to array
     samples = np.array(samples)
     target_labels = np.array(target_labels)
 
     return samples, target_labels
+
 
 def train_model(model, feature_vector_train, labels_train, feature_vector_valid, labels_valid, output_model_file):
     """
@@ -50,15 +52,16 @@ def train_model(model, feature_vector_train, labels_train, feature_vector_valid,
     model.fit(feature_vector_train, labels_train)
 
     # store the trained model
-    #pickle.dump(model, open(output_model_file, 'wb'))
-    
+    pickle.dump(model, open(output_model_file, 'wb'))
+
     # predict the labels on validation dataset
     predictions = model.predict(feature_vector_valid)
-    
+
     # get validation accuracy
     accuracy = metrics.accuracy_score(predictions, labels_valid)
 
     return accuracy
+
 
 def test_model(model_file, feature_vector_test, labels_test):
     """
@@ -75,41 +78,41 @@ def test_model(model_file, feature_vector_test, labels_test):
     # get the accuracy of the model on the test dataset
     accuracy = model.score(feature_vector_test, labels_test)
 
-    return accuracy    
+    return accuracy
+
 
 if __name__ == "__main__":
-    
-    #train_file = "../data/Amazon/amazon.cleaned.datasets/amazon.cleaned.vector/amazon.train.cleaned100%.vector.json"
-    #valid_file = "../data/Amazon/amazon.cleaned.datasets/amazon.cleaned.vector/amazon.valid.vector.json"
-    #test_file = "../data/Amazon/amazon.cleaned.datasets/amazon.cleaned.vector/amazon.test.vector.json"
-    
-    train_file = "../data/Yelp/yelp.cleaned.datasets/yelp.cleaned.vector/yelp.train.cleaned100%.vector.json"
-    valid_file = "../data/Yelp/yelp.cleaned.datasets/yelp.cleaned.vector/yelp.valid.vector.json"
-    test_file = "../data/Yelp/yelp.cleaned.datasets/yelp.cleaned.vector/yelp.test.vector.json"
-    
+
+    # train_file = "../data/Amazon/amazon.cleaned.datasets/amazon.cleaned.vector/amazon.train.cleaned100%.vector.json"
+    # valid_file = "../data/Amazon/amazon.cleaned.datasets/amazon.cleaned.vector/amazon.valid.vector.json"
+    # test_file = "../data/Amazon/amazon.cleaned.datasets/amazon.cleaned.vector/amazon.test.vector.json"
+
+    train_file = "./yelp20vec.json"
+    valid_file = "./yelp_val_vec.json"
+    test_file = "./yelp5vec.json"
 
     train_X, train_Y = read_dataset_file(train_file)
     valid_X, valid_Y = read_dataset_file(valid_file)
     test_X, test_Y = read_dataset_file(test_file)
 
     # manually grid search
-    model_folder = "../data/Yelp/yelp.trained.models"
-    
+    model_folder = "./models"
+
     # linear svc
-    #valid_acc_linear_svc = []
+    # valid_acc_linear_svc = []
     c_values = [0.01, 0.1, 1, 10, 100]
-    #for c in c_values:
+    # for c in c_values:
     #    model_file = "/linear_svc_" + str(c) + ".sav"    
     #    model = svm.LinearSVC(penalty='l1', dual=False, C=c, max_iter=1000)
     #    valid_accuracy = train_model(model, train_X, train_Y, valid_X, valid_Y, model_folder+model_file)
     #    valid_acc_linear_svc.append(valid_accuracy)
-    #print("valid_acc_linear_svc: ", valid_acc_linear_svc)
+    # print("valid_acc_linear_svc: ", valid_acc_linear_svc)
 
     # rbf kernel
     valid_acc_rbf_svc = []
     for c in c_values:
         model_file = "/rbf_svc_" + str(c) + ".sav"
         model = svm.SVC(C=c, kernel='rbf', max_iter=1000)
-        valid_accuracy = train_model(model, train_X, train_Y, valid_X, valid_Y, model_folder+model_file)
+        valid_accuracy = train_model(model, train_X, train_Y, valid_X, valid_Y, model_folder + model_file)
         valid_acc_rbf_svc.append(valid_accuracy)
     print("valid_acc_rbf_svc: ", valid_acc_rbf_svc)
