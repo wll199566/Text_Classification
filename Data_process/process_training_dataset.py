6ba326bc0149b27c33fcab87b9363aa3f520a83b
wll <wllnyubupt@gmail.com>
2018-12-20 12:09:57 -0500

    add process train for Yelp-full

--- Data_process/process_training_dataset.py
+++ Data_process/process_training_dataset.py
@@ -9,35 +9,44 @@ def prepare_train_dataset(dataset):
      Argument: dataset in {"amazon", "yelp"}
     """
     
-    if dataset == "amazon":
-        data_root_path = '../data/Amazon/'
-        train_output = "amazon.train.csv"
-        valid_output = "amazon.valid.csv"
-        labels = []
-        titles = []
-        contexts = []
-    else:
-        data_root_path = '../data/Yelp/'
-        train_output = "yelp.train.csv"
-        valid_output = "yelp.valid.csv"
-        labels = []
-        contexts = []
+    #if dataset == "amazon":
+    #    data_root_path = '../data/Amazon/'
+    #    train_output = "amazon.train.csv"
+    #    valid_output = "amazon.valid.csv"
+    #    labels = []
+    #    titles = []
+    #    contexts = []
+    #else:
+    #    data_root_path = '../data/Yelp/'
+    #    train_output = "yelp.train.csv"
+    #    valid_output = "yelp.valid.csv"
+    #    labels = []
+    #    contexts = []
+    data_root_path = '../data/Yelp-full'
+    train_output = "/yelp.full.train.csv"
+    valid_output = "/yelp.full.valid.csv"
+    labels = []
+    contexts = []
 
     # read in data from the original .csv file
-    with open(data_root_path + 'train.csv', 'r') as fin:
+    with open(data_root_path + '/train.csv', 'r') as fin:
         amazon_csv_train = csv.reader(fin)
         for row in amazon_csv_train:
             labels.append(row[0])
-            if dataset == "amazon":
-                titles.append(row[1])
-                contexts.append(row[2])
-            else: 
-                contexts.append(row[1])    
-
+            #if dataset == "amazon":
+            #    titles.append(row[1])
+            #    contexts.append(row[2])
+            #else: 
+            #    contexts.append(row[1])    
+            contexts.append(row[1].lower())
     # Combine the title and the contexts together
-    if dataset == "amazon":
-        for i in range(len(contexts)):
-            contexts[i] = (titles[i] + ' ' + contexts[i]).lower()
+    #if dataset == "amazon":
+    #    for i in range(len(contexts)):
+    #        contexts[i] = (titles[i] + ' ' + contexts[i]).lower()
+
+    # get rid of the header
+    labels = labels[1:]
+    contexts = contexts[1:]
 
     # split the training dataset into training and validataion datasets
     amazon_train_x, amazon_valid_x, amazon_train_y, amazon_valid_y = model_selection.train_test_split(contexts, labels)
@@ -63,6 +72,7 @@ def prepare_train_dataset(dataset):
 
 if __name__ == "__main__":
 
-    datasets = ["amazon", "yelp"]
-    for dataset in datasets:
-        prepare_train_dataset(dataset)  
+    #datasets = ["amazon", "yelp"]
+    #for dataset in datasets:
+    #    prepare_train_dataset(dataset)  
+    prepare_train_dataset("yelp-full")
